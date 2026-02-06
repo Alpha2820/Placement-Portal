7230de0f3884ebb413e41ecf5701e5c24d0966de
Abhijeet Tiwari <abhijeett709@gmail.com>
2026-02-07 05:02:42 +0530

    Fix API URL for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api';
+const API_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api';
 
 // Auth APIs
 export const registerUser = async (userData) => {
